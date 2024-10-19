cbf58b7088999ff970f7d9ecf00d036df470eeda
“Mauro-pixel” <espinal831@gmail.com>
2024-10-19 12:00:00 -0500

    conexion db en server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./config/db.js";
+
+
+dotenv.config();
 
 const app = express();
 
+app.get("/", (req,res)=>{
+  res.send("Servidor esta listo");
+});
+
 app.listen(8000, () => {
+  connectDB();
   console.log(`Servidor iniciado en http://localhost:8000`);
 });
